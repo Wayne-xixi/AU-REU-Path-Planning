130f13fffb2f4324bb4eabc399f60941adec419c
VColeman-games <35881660+VColeman-games@users.noreply.github.com>
2019-07-08 10:23:56 -0400

    Updated Code
    updated code and clearn it

--- WorkingWithOcTrees.cpp
+++ WorkingWithOcTrees.cpp
@@ -2,62 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ros/ros.h>
-//#include <octomap/octomap_timing.h>
 #include <octomap/octomap.h>
-//#include <octomap/math/Utils.h>
 #include <fstream>
-//#include <octomap/OcTreeLUT.h>
 #include <iostream>
 #include <istream>
 #include <list>
-//#include <octomap_msgs>
-//#include <octomap_msgs/conversion.h>
 #include <octomap/ColorOcTree.h>
 #include <octomap/OcTree.h>
 #include <octomap/OcTreeBase.h>
 #include <octomap/AbstractOcTree.h>
 #include <octomap/OcTreeIterator.hxx>
 #include<octomap/AbstractOccupancyOcTree.h>
-//#include <"power_plant.bt">
 
 using namespace std;
 using namespace octomap;
-//using namespace OcTree;
+
 
 int main(int argc, char** argv){
 ros::init (argc, argv, "OcTree_node");
 ros::start();
+int num_node = 1;
 
-// OcTreeBase* bt = OcTreeBase::read("power_plant.bt");
-/**
-ifstream fs;
-std::ofstream file("power_plant".c_str(), std::ios_base::in |std::ios_base::binary);
-fs.open("power_plant.bt");
-if (file.is_open()){
-ROS_INFO("File is open");
 
-}
-else{
-ROS_INFO("File is not open");
-
-}*/
-//OcTree* bt = new OcTree("power_plant.bt");
 AbstractOcTree* tree = AbstractOcTree::read("mapper.ot");
 OcTree* bt = dynamic_cast<OcTree*>(tree);
+
  
-  for(OcTree::leaf_iterator it = bt -> begin_leafs(), end = bt -> end_leafs(); it != end; ++it){
+ for(OcTree::leaf_iterator it = bt -> begin_leafs(), end = bt -> end_leafs(); it != end; ++it){
+	
 
     if(bt->isNodeOccupied(*it)){
-ROS_INFO("Node Occupied Nodes");
-//ROS_INFO( it.getX());
-//cout << "  x = " << it.getX() << std::endl;
-//ROS_INFO(*it);
-     // cout << "Node: " << *it << "is occupied" << "\n";
+	std::cout << "Node " << num_node << " is occupied. \n";
+        std::cout << "  x = " << it.getX() << std::endl;
+        std::cout << "  y = " << it.getY() << std::endl;
+        std::cout << "  z = " << it.getZ() << std::endl;
+	num_node ++;
     } else {
-      //cout << "Node: " << *it << "is not occupied" << "\n";
-//ROS_INFO("Following Nodes are NOT Occupied.");
-//ROS_INFO(*it);
+	std::cout << "Node " << num_node << " is free. \n";
+	std::cout << "  x = " << it.getX() << std::endl;
+        std::cout << "  y = " << it.getY() << std::endl;
+        std::cout << "  z = " << it.getZ() << std::endl;
+	num_node ++;
    }
+
   }
 ros::spin();
 ros::shutdown();
