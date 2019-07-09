acdd75b477f35ba54da79f9ebcd96ee2bc485bd6
VColeman-games <35881660+VColeman-games@users.noreply.github.com>
2019-07-09 17:13:44 -0500

    Update waypoint_test2.cpp

--- waypoint_test2.cpp
+++ waypoint_test2.cpp
@@ -1,147 +1,142 @@
-#include <ros/ros.h>
-#include <mavros_msgs/CommandHome.h>
-#include <mavros_msgs/CommandBool.h>
-#include <mavros_msgs/SetMode.h>
-#include <mavros_msgs/State.h>
-#include <mavros_msgs/WaypointPush.h>
-#include <mavros_msgs/WaypointClear.h>
-#include <std_msgs/String.h>
-#include <cstdlib>
-#include <mavros_msgs/Waypoint.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <list>
-#include <mavros_msgs/CommandCode.h>
-#include <mavros_msgs/State.h>
-
-mavros_msgs::State current_state;
-
-void state_cb(const mavros_msgs::State::ConstPtr& msg){
-    current_state = *msg;
-	bool connected = current_state.connected;
-	bool armed = current_state.armed;
-	ROS_INFO("%s", armed ? "" : "DisArmed");
-}
-
-int main (int argc, char **argv)
-{
-	  ros::init(argc, argv, "srv_waypoint");
-	  ros::NodeHandle nh;
-
-          mavros_msgs::SetMode auto_set_mode;
-          auto_set_mode.request.custom_mode = "AUTO.MISSION";
-    
-
-
-
-          ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, state_cb);
-          ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
-            ("mavros/set_mode");
-	  ros::ServiceClient wp_clear_client = nh.serviceClient<mavros_msgs::WaypointClear>("/mavros/mission/clear");
-          ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
-            ("mavros/cmd/arming");
-	  ros::ServiceClient wp_client = nh.serviceClient<mavros_msgs::WaypointPush>("/mavros/mission/push");
-	  ros::ServiceClient set_home_client = nh.serviceClient<mavros_msgs::CommandHome>("/mavros/cmd/set_home");
-/**
-	  mavros_msgs::WaypointPush wp_push_srv;
-	  mavros_msgs::WaypointClear wp_clear_srv;
-	  mavros_msgs::CommandHome set_home_srv;
-*/
- mavros_msgs::WaypointPush wp_push_srv; 
- mavros_msgs::Waypoint wp;
-
-// WP 0
-    wp.frame          = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
-    wp.command        = mavros_msgs::CommandCode::NAV_TAKEOFF;
-    wp.is_current     = true;
-    wp.autocontinue   = true;
-    wp.x_lat          = 47.3978206;
-    wp.y_long         = 8.543987;
-    wp.z_alt          = 20.0;
-    wp_push_srv.request.waypoints.push_back(wp);
-
-// WP 1
-    wp.frame          = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
-    wp.command        = mavros_msgs::CommandCode::NAV_LAND;
-    wp.is_current     = true;
-    wp.autocontinue   = true;
-    wp.x_lat          = 47.3978206;
-    wp.y_long         = 8.543987;
-    wp.z_alt          = 5.0;
-    wp_push_srv.request.waypoints.push_back(wp);
 /*
-    // WP 1
-    wp.frame          = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
-    wp.command        = mavros_msgs::CommandCode::NAV_LOITER_TIME;
-    wp.is_current     = false;
-    wp.autocontinue   = true;
-    wp.x_lat          = 47.3962527;
-    wp.y_long         = 8.5467917;
-    wp.z_alt          = 20;
-     wp.param1	      = 10;
-    wp.param3			= 2;
-	wp.param4			= 1;
-    wp_push_srv.request.waypoints.push_back(wp);
-    
-    // WP 2
-    wp.frame          = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
-    wp.command        = mavros_msgs::CommandCode::NAV_WAYPOINT;
-    wp.is_current     = false;
-    wp.autocontinue   = true;
-    wp.x_lat          = 49.3977783;
-    wp.y_long         = 8.547906;
-    wp.z_alt          = 20;
-    wp_push_srv.request.waypoints.push_back(wp);
+ * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-    // WP 3
-    wp.frame          = mavros_msgs::Waypoint::FRAME_MISSION;
-    wp.command        = mavros_msgs::CommandCode::NAV_RETURN_TO_LAUNCH;
-    wp.is_current     = false;
-    wp.autocontinue   = true;
-    wp.x_lat          = 0;
-    wp.y_long         = 0;
-    wp.z_alt          = 0;
-    wp_push_srv.request.waypoints.push_back(wp);**/
+#include <fstream>
+#include <iostream>
 
-    ros::Rate rate(20.0);
+#include <Eigen/Geometry>
+#include <mav_msgs/conversions.h>
+#include <mav_msgs/default_topics.h>
+#include <mav_msgs/eigen_mav_msgs.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
+using namespace std;
 
-    // wait for FCU connection
-    while(ros::ok() && current_state.connected){
-        ros::spinOnce();
-        rate.sleep();
-    }
-    ROS_INFO("Connected to PX4!");
-    // ARM
-    mavros_msgs::CommandBool arm_cmd;
-    arm_cmd.request.value = true;
-    if (arming_client.call(arm_cmd) &&
-        arm_cmd.response.success){
-        ROS_INFO("Vehicle armed");
-    }
+bool sim_running = false;
 
-    // Send WPs to Vehicle
-    if (wp_client.call(wp_push_srv)) {
-        ROS_INFO("Send waypoints ok: %d", wp_push_srv.response.success);
-        if (current_state.mode != "AUTO.MISSION") {
-            if( set_mode_client.call(auto_set_mode) &&
-                auto_set_mode.response.mode_sent){
-                ROS_INFO("AUTO.MISSION enabled");
-            }
-        }
-    }
-    else
-        ROS_ERROR("Send waypoints FAILED.");
+static const int64_t kNanoSecondsInSecond = 1000000000;
 
-    while(ros::ok()){
-        ros::spinOnce();
-        rate.sleep();
-    }
-
-    return 0;
+void callback(const sensor_msgs::ImuPtr& msg) {
+  sim_running = true;
 }
 
+class WaypointWithTime {
+ public:
+  WaypointWithTime()
+      : waiting_time(0), yaw(0.0) {
+  }
 
+  WaypointWithTime(double t, float x, float y, float z, float _yaw)
+      : position(x, y, z), yaw(_yaw), waiting_time(t) {
+  }
 
-	  
+  Eigen::Vector3d position;
+  double yaw;
+  double waiting_time;
+};
 
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "waypoint_publisher");
+  ros::NodeHandle nh;
 
+  ROS_INFO("Started waypoint_publisher.");
+
+  ros::V_string args;
+  ros::removeROSArgs(argc, argv, args);
+
+  if (args.size() != 2 && args.size() != 3) {
+    ROS_ERROR("Usage: waypoint_publisher <waypoint_file>"
+        "\nThe waypoint file should be structured as: space separated: wait_time [s] x[m] y[m] z[m] yaw[deg])");
+    return -1;
+  }
+
+  std::vector<WaypointWithTime> waypoints;
+  const float DEG_2_RAD = M_PI / 180.0;
+
+  std::ifstream wp_file(args.at(1).c_str());
+
+  if (wp_file.is_open()) {
+    double t, x, y, z, yaw;
+    // Only read complete waypoints.
+    while (wp_file >> t >> x >> y >> z >> yaw) {
+      waypoints.push_back(WaypointWithTime(t, x, y, z, yaw * DEG_2_RAD));
+    }
+    wp_file.close();
+    ROS_INFO("Read %d waypoints.", (int) waypoints.size());
+  } else {
+    ROS_ERROR_STREAM("Unable to open poses file: " << args.at(1));
+    return -1;
+  }
+
+  // The IMU is used, to determine if the simulator is running or not.
+  ros::Subscriber sub = nh.subscribe("imu", 10, &callback);
+
+  ros::Publisher wp_pub =
+      nh.advertise<trajectory_msgs::MultiDOFJointTrajectory>(
+      mav_msgs::default_topics::COMMAND_TRAJECTORY, 10);
+
+  ROS_INFO("Wait for simulation to become ready...");
+
+  while (!sim_running && ros::ok()) {
+    ros::spinOnce();
+    ros::Duration(0.1).sleep();
+  }
+
+  ROS_INFO("...ok");
+
+  // Wait for 30s such that everything can settle and the mav flies to the initial position.
+  ros::Duration(30).sleep();
+
+  ROS_INFO("Start publishing waypoints.");
+  for (int i = 0; i < waypoints.size(); i++)
+  {
+     cout << "Waypoint %d: " << i+1 << endl;
+     cout << "Wait Time %d: " << waypoints[i].t << endl;
+     cout << "X %d: " << waypoints[i].position(x) << endl;
+     cout << "Y %d: " << waypoints[i].position(y) << endl;
+     cout << "Z %d: " << waypoints[i].position(z) << endl;
+     cout << "Yaw %d: " << waypoints[i].yaw << endl;
+
+ }
+/*
+  trajectory_msgs::MultiDOFJointTrajectoryPtr msg(new trajectory_msgs::MultiDOFJointTrajectory);
+  msg->header.stamp = ros::Time::now();
+  msg->points.resize(waypoints.size());
+  msg->joint_names.push_back("base_link");
+  int64_t time_from_start_ns = 0;
+  for (size_t i = 0; i < waypoints.size(); ++i) {
+    WaypointWithTime& wp = waypoints[i];
+
+    mav_msgs::EigenTrajectoryPoint trajectory_point;
+    trajectory_point.position_W = wp.position;
+    trajectory_point.setFromYaw(wp.yaw);
+    trajectory_point.time_from_start_ns = time_from_start_ns;
+
+    time_from_start_ns += static_cast<int64_t>(wp.waiting_time * kNanoSecondsInSecond);
+
+    mav_msgs::msgMultiDofJointTrajectoryPointFromEigen(trajectory_point, &msg->points[i]);
+  }
+  wp_pub.publish(msg);*/
+
+  ros::spinOnce();
+  ros::shutdown();
+
+  return 0;
+}
