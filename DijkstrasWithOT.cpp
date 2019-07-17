c1f04e3dffb144a848980ee6027879bc325a2375
christinadawns <51244233+christinadawns@users.noreply.github.com>
2019-07-17 12:39:51 -0500

    Update DijkstrasWithOT.cpp

--- DijkstrasWithOT.cpp
+++ DijkstrasWithOT.cpp
@@ -37,6 +37,7 @@ struct PointCost
   int x, y, z;
   float cost;
 };
+
 //Keeps track of future movements from current node
 struct Front
 {
@@ -45,6 +46,7 @@ struct Front
   Point previous;
 };
 
+//Comparator to compare Front structs by cost
 class myComparator
 {
 public:
@@ -56,36 +58,34 @@ public:
 
 //Create Dijkstra's algorithm
 vector<Point> Dijkstras(Point start, Point goal, OcTree* octo) {
+   //Time the function
    clock_t t;
    t=clock();
-  //Initialize to first node which has cost 0, it's only option is start, and it came from no other nodes
-  //Front front[?] = {{.00001, start, NULL}};
+  //The size of our environment
   int min_x = -36;
   int max_x = 36;
   int min_y = -36;
   int max_y = 36;
   int min_z = 0;
   int max_z = 72;
+  //Create a priority queue to keep track of Front objects
   priority_queue <Front, vector<Front>, myComparator > front;
+  //Initialize to first node which has cost 0, it's only option is start, and it came from no other nodes
   front.push({.00001, start, {0,0,0}});
-  //Initialize visited array to be the size of all of the nodes Initialized all to zeroes
+  //Find dimensions of environment
   int env_x = abs(min_x)+max_x;
   int env_y = abs(min_y)+max_y;
   int env_z = abs(min_z)+max_z;
+  //Initialize visited array to be the size of all of the nodes Initialized all to zeroes
   double visited[env_x][env_y][env_z] = {0};
   //Create an array that keeps track of what each point came from
   Point came_from[env_x][env_y][env_z];
   //Initialize the possible movements which contain (x,y,z) points and the cost to move to those points
   PointCost movements[6] = {{1,0,0,1},{0,1,0,1},{-1,0,0,1},{0,-1,0,1},{0,0,1,1},{0,0,-1,1}};
   Point pos_of_node;
-  //Find the node with the minimum cost (not using a heap)
+  //While we haven't searched every node and we haven't reached the goal
   while(front.size()>0){
-/*
-    cout << "What is in the front?" << endl;
-    for(int i = 0; i < front.size(); i++){
-	cout << front.size() << endl;
-    }
-*/
+
     //remove the node with the minimum cost
     Front min_node = front.top();
     front.pop();
@@ -96,7 +96,7 @@ vector<Point> Dijkstras(Point start, Point goal, OcTree* octo) {
     pos_of_node = min_node.pos;
     Point previous_node = min_node.previous;
 
-    //Check if this node has been visited before
+    //Account for negative numbers when indexing arrays
     int x_array = pos_of_node.x;
     if(pos_of_node.x < 0){
     	x_array = abs(pos_of_node.x)+max_x-1;
@@ -105,12 +105,11 @@ vector<Point> Dijkstras(Point start, Point goal, OcTree* octo) {
     if(pos_of_node.y < 0){
     	y_array = abs(pos_of_node.y)+max_y-1;
     }
-
+    //If the node was visited, go back to beginning of while loop
     if (visited[x_array][y_array][pos_of_node.z] > 0){
       continue;
     }
     //if it was not visited, it now is so add it to the visited array
-
     visited[x_array][y_array][pos_of_node.z] = cost_of_node;
     //update the came_from array to show the previous node that is associated with this node
     came_from[x_array][y_array][pos_of_node.z] = previous_node;
@@ -124,9 +123,11 @@ vector<Point> Dijkstras(Point start, Point goal, OcTree* octo) {
     int new_z = 0;
     //Checking the neighbors
     for(int i = 0; i < 6; i++){
+      //Creating a new neighboring node
       new_x=pos_of_node.x+movements[i].x;
       new_y=pos_of_node.y+movements[i].y;
       new_z=pos_of_node.z+movements[i].z;
+      //Checking that the node is within our environment
       if(new_x<min_x || new_x >= max_x || new_y<min_y || new_y>=max_y || new_z<min_z || new_z>=max_z){
         continue;
       }
@@ -142,33 +143,43 @@ vector<Point> Dijkstras(Point start, Point goal, OcTree* octo) {
       if(new_pos.y < 0){
     	  new_y_array = abs(new_pos.y)+max_y-1;
       }
+      //Convert to double for search function
       double temp_x = new_pos.x;
       double temp_y = new_pos.y;
       double temp_z = new_pos.z;
+      //Search for the node in the octomap
       OcTreeNode* node = octo->search(temp_x, temp_y, temp_z, 0);
       int occupied = 0;
-
+      //If the node doesn't exist (node == 0) assume that it is unoccupied
+      //Check that the node doesn't exist and is unvisited
       if(node == 0 && visited[new_x_array][new_y_array][new_pos.z]==0 ){
+	//Check nodes within .8 meters of node to ensure that there is enough space for the drone to fly
 	for(OcTree::leaf_bbx_iterator it = octo -> begin_leafs_bbx(Vector3 (new_pos.x-0.8,new_pos.y-0.8, new_pos.z-0.8), Vector3 (new_pos.x+ 0.8,new_pos.y+0.8, new_pos.z+0.8)), end = octo -> end_leafs_bbx(); it != end; ++it){
                 occupied = octo ->isNodeOccupied(*it);
+                //If a node is occupied, break the loop
  		if(occupied == 1){
 		  break;
 		}
 
         }
+        //If no surrounding nodes are occupied, push the node onto the front
 	if(occupied == 0){
 	   front.push({cost_of_node+movements[i].cost, new_pos, pos_of_node});
 
         }
 
        }
+      //If the node is unoccupied and unvisited
       else if(visited[x_array][y_array][new_pos.z]==0 && octo->isNodeOccupied(node) ==0){
+        //Check nodes within .8 meters of node to ensure that there is enough space for the drone to fly
 	for(OcTree::leaf_bbx_iterator it = octo -> begin_leafs_bbx(Vector3 (new_pos.x-0.8,new_pos.y-0.8, new_pos.z-0.8), Vector3 (new_pos.x+ 0.8,new_pos.y+0.8, new_pos.z+0.8)), end = octo -> end_leafs_bbx(); it != end; ++it){
                 occupied = octo ->isNodeOccupied(*it);
+		//If a node is occupied, break the loop
  		if(occupied == 1){
 	          break;
 		}
         }
+        //If no surrounding nodes are occupied, push the node onto the front
 	if(occupied == 0){
 	   front.push({cost_of_node+movements[i].cost, new_pos, pos_of_node});
 
@@ -176,11 +187,11 @@ vector<Point> Dijkstras(Point start, Point goal, OcTree* octo) {
       }
     }
   }
-  //cout << "Break the loop" << endl;
   //Initialize optimal_path array which contains (x,y,z) points
   vector<Point> path;
+  //If we broke out of the loop because we found the goal
   if(pos_of_node.x == goal.x && pos_of_node.y == goal.y && pos_of_node.z == goal.z){
-    //cout << "In the if statement" << endl;
+    //while our current position isn't the start position
     while(pos_of_node.x != start.x || pos_of_node.y != start.y || pos_of_node.z != start.z){
       int x_array = pos_of_node.x;
       if(pos_of_node.x < 0){
@@ -190,40 +201,41 @@ vector<Point> Dijkstras(Point start, Point goal, OcTree* octo) {
       if(pos_of_node.y < 0){
     	  y_array = abs(pos_of_node.y)+max_y-1;
       }
+      //Add the current node into the path vector
       path.push_back(pos_of_node);
-      //cout << "adding: " << pos_of_node.x << ", " << pos_of_node.y << ", " << pos_of_node.z << endl;
+      //change our current position to be the node that our previous node came from
       pos_of_node = came_from[x_array][y_array][pos_of_node.z];
     }
+    //Add the start node to our vector
     path.push_back(start);
+    //reverse the vector
     reverse(path.begin(), path.end());
   } else {
        cout << "Your goal is unreachable" << endl;
     }
+    //End the timer
     t = clock()-t;
     cout <<"This took : " << (float)t/CLOCKS_PER_SEC << " seconds" << endl;
   return path;
 }
 
 int main(){
+  //Initialize the start and goal nodes
   Point start = {0,0,1};
   Point goal = {5,0,1};
+  //read in the octomap
   AbstractOcTree* tree = AbstractOcTree::read("obstaclecourse2.ot");
   OcTree* bt = dynamic_cast<OcTree*>(tree);
-  //in python code they create an array of all zeros that will contain a value of 255 if an obstacle is there
-  //world_obstacles = np.zeros(world_extents, dtype=np.uint8)
-  //int obstacles_in_env[env_x][env_y][env_z] = {0};
-  //If there is an obstacle at a node, set that node to be 255
-  //STILL NEED TO CODE WHEN THE TIME COMES
-
+  //Call Dijkstras
   vector<Point> results;
   results = Dijkstras(start, goal, bt);
   cout << "Size of Results : "<< results.size() << endl;
   cout << "The Goals is ( " << goal.x << " ," <<goal.y<< " ,"<<goal.z <<" )"<< endl; 
-  ofstream outfile ("Waypoints_DJ.txt");
+  //Create an output file which contains all of the waypoints
+  ofstream outfile ("Waypoints_DJOT.txt");
   for (int i = 1; i < results.size(); i ++){
-  outfile << "0 " << results[i].x << " " << results[i].y << " " << results[i].z << " " << "0" << endl;
+    outfile << "0 " << results[i].x << " " << results[i].y << " " << results[i].z << " " << "0" << endl;
   }
-outfile.close();
-  //Use waypoint script information to actually utilize the Points in our vector to move from point to point
-
+  outfile.close();
+  //Use waypoint script information to actually utilize the Points in our file to move from point to point
 }
